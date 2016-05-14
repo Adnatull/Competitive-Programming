9182bb8c198be444c6aebf3ae21a2cc23b8472e9
Zip Light <zip.light68@gmail.com>
2016-05-14 22:38:00 +0600

    g

---
+++ UVA Online Judge/11945.cpp
@@ -0,0 +1,16 @@
+#include<cstdio>
+
+#define input scanf
+#define print printf
+#define zokhon while
+#define deci int
+#define doshomik double
+#define ferot return
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
