0b9e8550403568cdc9d9d5f01bc6f847d16f1b66
Mohd Adnatull Al Masum <Adnatull@users.noreply.github.com>
2017-06-05 19:22:58 +0600

    Add files via upload

---
+++ Light OJ/1087 - Diablo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+struct node {
+
+    int key;
+    struct node *left, *right;
+};
