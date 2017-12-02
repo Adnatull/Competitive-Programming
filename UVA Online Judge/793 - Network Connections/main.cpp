6946eb5c17cfaf23656a44818acfb1f0b8902351
Adnatull <adnaan.masum@yahoo.com>
2017-12-02 23:54:21 +0600

    Union Finder Algorithm

---
+++ UVA Online Judge/793 - Network Connections/main.cpp
@@ -0,0 +1,139 @@
+	
+/*
+ * Adnatull
+ * 
+ * Mail: adnaan.masum@outlook.com
+ * Date: 2017-12-02
+ * */
+ 
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
+#include <map>
+#include <climits>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <sstream>
+#include <stack>
+#include <list>
+#include <deque>
+#include <cmath>
+#include <numeric>
+#include <iterator>
+#include <utility>
+
+
+using namespace std;
+
+#define sci(a) scanf ("%d",&a)
+#define scii(a,b) scanf ("%d %d",&a,&b)
+#define sc scanf
+#define pr printf 
+#define pb push_back
+#define mp make_pair
+#define sz(a) (int)a.size()
+#define LL long long 
+#define L long 
+#define FOR(i,a,b) for (int i=(a); i<=(b); i++)
+#define RFOR(i,a,b) for (int i=(a); i>=(b); i--)
+#define rep(i,n) for (int i=(0); i<(n); i++)
+#define rrep(i,n) for (int i=(n-1); i>=(0); i--)
+#define INF (int)1e9+1
+#define EPS (int)1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD (int)1000000007
+#define nn printf ("\n")
+#define fill(a, v) memset(a, v, sizeof a)
+#define clr(a) memset(a, 0, sizeof a)
+#define ff first
+#define ss second
+#define all(a) a.begin(),a.end()
+#define skip cin.ignore()
+#define Global (int)12345678
+#define Local (int)1234567
+#define test_case(n) FOR(cases,1,n)
+#define print_case printf("Case %d: ",cases)
+
+const double pi=acos(-1.0);
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mpii;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+
+int parent[1234567];	
+
+
+int fin (int r) { 
+	if(parent[r]==r) return r;
+	return parent[r] = fin(parent[r]);	
+}
+
+void unio (int x, int y) { 
+	
+		int u = fin(x);
+		int v = fin(y);
+		
+		parent[u] = parent[v];
+}
+
+int main() { 
+	
+	#ifndef ONLINE_JUDGE
+		freopen("in.txt","r",stdin);
+		//freopen("out.txt","w",stdout);
+	#endif
+	
+	//ios_base::sync_with_stdio(0);
+
+	int t;
+	sci(t);
+	cin.ignore();
+
+	int cases = 0;
+	string s;
+	int plus=0,minus=0;
+	while (cases<=t) { 
+		getline(cin,s);
+		if(s==""){ 
+			if(cases>0){ 
+				pr("%d,%d\n",plus,minus);
+				if(cases+1<=t) nn;
+			}
+			plus=0,minus=0;
+			cases++; }
+		else if ( isdigit(s[0]) ) { 
+			int n =stoi(s);
+			FOR(i,0,n) parent[i] = i;
+		}
+		else { 
+			char c;
+			int x,y;
+			stringstream ss(s);
+			ss >> c;
+			ss >> x;
+			ss >> y;
+			if(c=='c') unio(x,y);
+			else { 
+				x = fin(x);
+				y = fin(y);
+				if (x==y) plus++;
+				else minus++;
+			}
+		}
+
+	return 0;
+}
