a1aedf5a4fdbf0fd350dc2d54d18aaeecbba2abf
Mohd Adnatull Al Masum <Adnatull@users.noreply.github.com>
2018-02-14 02:06:34 +0600

    10878 - Decode the tape

---
+++ UVA Online Judge/Python/10878 - Decode the tape/10878 - Decode the tape.py
@@ -0,0 +1,219 @@
+def build( s ):
+    if s == "| o   .  o|":
+        print('A', end='')
+    elif s == "|  o  .   |":
+        print (' ', end='')
+    elif s == "| ooo .  o|":
+        print ('q', end='')
+    elif s == "| ooo .o o|":
+        print ('u', end='')
+    elif s == "| oo o.  o|":
+        print ('i', end='')
+    elif s == "| oo  . oo|":
+        print ('c', end='')
+    elif s == "| oo o. oo|":
+        print ('k', end='')
+    elif s == "| oo  . o |":
+        print ('b', end='')
+    elif s == "| ooo . o |":
+        print ("r", end='')
+    elif s == "| oo o.ooo|":
+        print ("o", end='')
+    elif s == "| ooo .ooo|":
+        print ("w", end='')
+    elif s == "| oo o.oo |":
+        print ("n", end='')
+    elif s == "| oo  .oo |":
+        print ("f", end='')
+    elif s == "| oooo.   |":
+        print ('x', end='')
+    elif s == "| oo o. o |":
+        print ('j', end='')
+    elif s == "| ooo .o o|":
+        print ('u', end='')
+    elif s == "| oo o.o o|":
+        print ('m', end='')
+    elif s == "| ooo .   |":
+        print ('p', end='')
+    elif s == "| ooo . oo|":
+        print ('s', end='')
+    elif s == "| ooo .oo |":
+        print ("v", end='')
+    elif s == "| oo  .o o|":
+        print ('e', end='')
+    elif s == "| ooo .o  |":
+        print ('t', end='')
+    elif s == "| oo o.   |":
+        print ('h', end='')
+    elif s == "| oo  .o o|":
+        print ('e', end='')
+    elif s == "| oo o.o  |":
+        print ('l', end='')
+    elif s == "| oo  .  o|":
+        print ('a', end='')
+    elif s == "| oooo. o |":
+        print ('z', end='')
+    elif s == "| oooo.  o|":
+        print ('y', end='')
+    elif s == "| oo  .o  |":
+        print ('d', end='')
+    elif s == "| oo o.ooo|":
+        print ('o', end='')
+    elif s == "| oo  .ooo|":
+        print ('g', end='')
+    elif s == "|  o o.oo |":
+        print ('.', end='')
+    elif s == "|    o. o |":
+        print('')
+    elif s == "|  o  . o |":
+        print ("\"", end='')
+    elif s == "|  oo .   |":
+        print ("0", end='')
+    elif s == "|  oo .  o|":
+        print ("1", end='')
+    elif s == "|  oo . o |":
+        print ("2", end='')
+    elif s == "|  oo . oo|":
+        print ("3", end='')
+    elif s == "|  oo .o  |":
+        print ("4", end='')
+    elif s == "|  oo .o o|":
+        print ("5", end='')
+    elif s == "|  oo .oo |":
+        print ("6", end='')
+    elif s == "|  oo .ooo|":
+        print ("7", end='')
+    elif s == "|  ooo.   |":
+        print ("8", end='')
+    elif s == "|  ooo.  o|":
+        print ("9", end='')
+    elif s == "| o   . o |":
+        print ("B", end='')
+    elif s == "| o   . oo|":
+        print ("C", end='')
+    elif s == "| o   .o  |":
+        print ("D", end='')
+    elif s == "| o   .o o|":
+        print ("E", end='')
+    elif s == "| o   .oo |":
+        print ("F", end='')
+    elif s == "| o   .ooo|":
+        print ("G", end='')
+    elif s == "| o  o.   |":
+        print ("H", end='')
+    elif s == "| o  o.  o|":
+        print ("I", end='')
+    elif s == "| o  o. o |":
+        print ("J", end='')
+    elif s == "| o  o. oo|":
+        print ("K", end='')
+    elif s == "| o  o.o  |":
+        print ("L", end='')
+    elif s == "| o  o.o o|":
+        print ("M", end='')
+    elif s == "| o  o.oo |":
+        print ("N", end='')
+    elif s == "| o  o.ooo|":
+        print ("O", end='')
+    elif s == "| o o .   |":
+        print ("P", end='')
+    elif s == "| o o .  o|":
+        print ("Q", end='')
+    elif s == "| o o . o |":
+        print ("R", end='')
+    elif s == "| o o . oo|":
+        print ("S", end='')
+    elif s == "| o o .o  |":
+        print ("T", end='')
+    elif s == "| o o .o o|":
+        print ("U", end='')
+    elif s == "| o o .oo |":
+        print ("V", end='')
+    elif s == "| o o .ooo|":
+        print ("W", end='')
+    elif s == "| o oo.   |":
+        print ("X", end='')
+    elif s == "| o oo.  o|":
+        print ("Y", end='')
+    elif s == "| o oo. o |":
+        print ("Z", end='')
+    elif s == "| o oo.o  |":
+        print ('\\', end='')
+    elif s == "|  ooo. o |":
+        print (':', end='')
+    elif s == "|  ooo. oo|":
+        print (';', end = '')
+    elif s == "|  ooo.o  |":
+        print ('<', end = '')
+    elif s == "|  ooo.o o|":
+        print ('=', end = '')
+    elif s == "|  ooo.oo |":
+        print ('>', end = '')
+    elif s == "|  ooo.ooo|":
+        print ('?', end = '')
+    elif s == "| o   .   |":
+        print ('@', end = '')
+    elif s == "| o oo. oo|":
+        print ('[', end = '')
+    elif s == "| o oo.o o|":
+        print (']', end = '')
+    elif s == "| o oo.oo |":
+        print ('^', end = '')
+    elif s == "| o oo.ooo|":
+        print ('_', end = '')
+    elif s == "| oo  .   |":
+        print ('`', end = '')
+    elif s == "|    o.o o|":
+        print ("")
+    elif s == "|  o  .  o|":
+        print ('!', end = '')
+    elif s == "|  o  . oo|":
+        print ("#", end = '')
+    elif s == "|  o  .o  |":
+        print ('$', end = '')
+    elif s == "|  o  .o o|":
+        print ('%', end = '')
+    elif s == "|  o  .oo |":
+        print ('&', end = '')
+    elif s == "|  o  .ooo|":
+        print ('\'', end = '')
+    elif s == "|  o o.   |":
+        print ('(', end = '')
+    elif s == "|  o o.  o|":
+        print (')', end = '')
+    elif s == "|  o o. o |":
+        print ('*', end = '')
+    elif s == "|  o o. oo|":
+        print ('+', end = '')
+    elif s == "|  o o.o  |":
+        print (',', end = '')
+    elif s == "|  o o.o o|":
+        print ('-', end = '')
+    elif s == "|  o o.ooo|":
+        print ('/', end = '')
+    elif s == "| oooo. oo|":
+        print ('{', end = '')
+    elif s == "| oooo.o  |":
+        print ('|', end = '')
+    elif s == "| oooo.o o|":
+        print ('}', end = '')
+    elif s == "| oooo.oo |":
+        print ('~', end = '')
+   # elif s == "|    o.   |":
+      #  print ('', end = '')
+    elif s == "|    o.  o|":
+        print ('	', end = '')
+    return
+
+#import sys
+#sys.stdin = open('input.txt', 'r')
+
+while True:
+    try:
+        s = input()
+        build(s)
+
+    except EOFError:
+        break
+
+
